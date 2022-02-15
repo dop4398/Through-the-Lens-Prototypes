f13ee88ea8da30e13f0815da538739c450ad2fea
Abby <abmarkish16@yahoo.com>
2022-02-15 13:08:47 -0500

    Continuing Level Design GOOD
    :)

--- Prototype URP and Sound/Assets/Scripts/Interactables/ItemPickup.cs
+++ Prototype URP and Sound/Assets/Scripts/Interactables/ItemPickup.cs
@@ -11,18 +11,14 @@ using UnityEngine;
 public class ItemPickup : MonoBehaviour, IInteractable
 {
     #region Fields
-    public float radius { get; set; }
-    public string ID; // unique identifier for each item
+    public ItemDatabase itemData;
+    //public float radius { get; set; }
+    public string name;
+    public int ID; // unique identifier for each item
     #endregion
 
     void Start()
     {
-        radius = 2.0f;
-
-        if (ID == null)
-        {
-            ID = this.name;
-        }
     }
 
     void Update()
@@ -31,31 +27,13 @@ public class ItemPickup : MonoBehaviour, IInteractable
     }
 
     #region Helper Methods
-    public void OnMouseOver()
-    {
-        if (Vector3.Distance(CharacterComponents.instance.controller.transform.position, this.gameObject.transform.position) <= radius)
-        {
-            // Call UI method here
-
-            if(PlayerInput.playerInput.interact)
-            {
-                Interaction();
-            }
-        }
-    }
-
-    public void OnMouseExit()
-    {
-        // Turn off UI popup here
-    }
-
     /// <summary>
     /// Put it in the bag.
     /// </summary>
     public void Interaction()
     {
         // Add the item to the inventory
-
+        CharacterComponents.instance.controller.GetComponent<CollectableInventory>().GiveItem(ID);
         // Set inactive in the scene
         this.gameObject.SetActive(false);
     }
