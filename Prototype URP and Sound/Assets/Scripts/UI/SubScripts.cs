9af79e2fa98cd9e11a2bf8e237d5dbe6225ae1ce
Abby <abmarkish16@yahoo.com>
2022-04-05 15:10:00 -0400

    Asset Cleanup and Street Fair Expansion

--- Prototype URP and Sound/Assets/Scripts/UI/SubScripts.cs
+++ Prototype URP and Sound/Assets/Scripts/UI/SubScripts.cs
@@ -13,13 +13,18 @@ public class SubScripts : MonoBehaviour
     public GameObject textBox;
     StreamReader reader = null;
     string fileName;
+    string tutorialFile;
     public int x = 0;
     //string[][] dialogueText;
+    List<String>[] tutorialText = new List<String>[100];
+    List<int>[] tutorialTimers = new List<int>[100];
+    int[] tutorialStopCount = new int[10];
     List<String>[] dialogueText = new List<String>[100];
     List<int>[] timers = new List<int>[100];
     int[] stationCount = new int[10];
-  
+
     public int lineCount;
+    public int tutLineCount;
 
     private void Awake()
     {
@@ -29,39 +34,57 @@ public class SubScripts : MonoBehaviour
     private void Start()
     {
         fileName = Path.Combine(Application.streamingAssetsPath, "dialogueText.txt");
+        tutorialFile = Path.Combine(Application.streamingAssetsPath, "tutorialText.txt");
         //These need to be changed to a better option than hardcoding :)
-      
+
 
         //Make sure everything that needs to be "empty" is
         textBox.GetComponent<Text>().text = "";
         int station = 0;
         int tempInt = 0;
-        int count = -1;
+        int count = 0;
         lineCount = 0;
+        tutLineCount = 0;
 
         //get the number of lines in the text file
-        try {
+        try
+        {
             reader = new StreamReader(fileName);
             while (reader.ReadLine() != null)
-            { 
-                lineCount++; 
+            {
+                lineCount++;
 
             }
             reader.Close();
         }
-        catch (Exception e) {
+        catch (Exception e)
+        {
+            Debug.Log("Cannot Read the File: " + e);
+        }
+        try
+        {
+            reader = new StreamReader(tutorialFile);
+            while (reader.ReadLine() != null)
+            {
+                tutLineCount++;
+
+            }
+            reader.Close();
+        }
+        catch (Exception e)
+        {
             Debug.Log("Cannot Read the File: " + e);
         }
 
         //loop through the file more slowly now and separate the information correctly
         try
         {
-            
+
             reader = new StreamReader(fileName);
-            for (int i = 0;i< lineCount; i++)
+            for (int i = 0; i < lineCount; i++)
             {
                 string[] temp = reader.ReadLine().Split('|');
-                
+
                 //add station number
                 //count how many for station number
                 //if next station number is different, add count to station's second spot
@@ -72,7 +95,7 @@ public class SubScripts : MonoBehaviour
                     station = tempInt;
                     count = 0;
                 }
-                else 
+                else
                 {
                     count++;
                 }
@@ -80,13 +103,12 @@ public class SubScripts : MonoBehaviour
 
                 //add dialogue to string array
                 //play just as many lines as there are in the station
-                //BREAKS HERE
                 if (dialogueText[station] == null)
                 {
                     dialogueText[station] = new List<string>();
                 }
                 dialogueText[station].Add(temp[1].Trim());
-                //Debug.Log("Finished Text");
+
 
 
                 //add each strings time to the array
@@ -98,23 +120,83 @@ public class SubScripts : MonoBehaviour
                     timers[station] = new List<int>();
                 }
                 timers[station].Add(tempInt);
-                //Debug.Log("Finished Timers");
 
-                //Debug.Log("Station 0's count: " + stationCount[0]);
 
             }
-            
+
         }
-        catch(Exception e)
+        catch (Exception e)
         {
-            //Debug.Log("Something went wrong with reading the file: " + e);
-            
+            Debug.Log("Something went wrong with reading the file: " + e);
+
         }
         finally
         {
             reader.Close();
         }
-        //stationCount[0] = 5
+        /////////////////////////////////////////////////////////////////////////////////////
+        ////Tutorial Start//////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////
+        try
+        {
+            tempInt = 0;
+            station = 0;
+            count = 0;
+            reader = new StreamReader(tutorialFile);
+            for (int i = 0; i < tutLineCount; i++)
+            {
+                string[] temp = reader.ReadLine().Split('|');
+
+                //add station number
+                //count how many for station number
+                //if next station number is different, add count to station's second spot
+                int.TryParse(temp[0].Trim(), out tempInt);
+                if (tempInt != station)
+                {
+                    tutorialStopCount[station] = count;
+                    station = tempInt;
+                    count = 0;
+                }
+                else
+                {
+                    count++;
+                }
+
+
+                //add dialogue to string array
+                //play just as many lines as there are in the station
+                if (tutorialText[station] == null)
+                {
+                    tutorialText[station] = new List<string>();
+                }
+                tutorialText[station].Add(temp[1].Trim());
+
+
+
+                //add each strings time to the array
+                //and loop through the number of stations to get it
+                int.TryParse(temp[2].Trim(), out tempInt);
+
+                if (tutorialTimers[station] == null)
+                {
+                    tutorialTimers[station] = new List<int>();
+                }
+                tutorialTimers[station].Add(tempInt);
+
+
+            }
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Something went wrong with reading the file: " + e);
+
+        }
+        finally
+        {
+            reader.Close();
+        }
+
     }
 
 
@@ -122,6 +204,11 @@ public class SubScripts : MonoBehaviour
     {
         StopAllCoroutines();
         StartCoroutine(TextSequence(x));
+    }
+    public void tutorialRun(int x)
+    {
+        StopAllCoroutines();
+        StartCoroutine(TutorialSequence(x));
     }
 
     //testing out the text showing up
@@ -140,7 +227,7 @@ public class SubScripts : MonoBehaviour
         textBox.GetComponent<Text>().text = "It helps draw a line between our safe haven and the outside";
         yield return new WaitForSeconds(5);
         textBox.GetComponent<Text>().text = "";
-    
+
     }
 
     //takes the int provided from the trigger box, looks in the file for those lines, and returns them correctly :)
@@ -148,16 +235,25 @@ public class SubScripts : MonoBehaviour
     IEnumerator TextSequence(int x)
     {
         //yield return new WaitForSeconds(1);
-
         //loop through the given station's line count, change the subtitles based on the necessary text and with the right time length
         //StationCount[0] = 0
         for (int i = 0; i <= stationCount[x]; i++)
         {
-            
             textBox.GetComponent<Text>().text = dialogueText[x][i];
             yield return new WaitForSeconds(timers[x][i]);
         }
-
         textBox.GetComponent<Text>().text = "";
     }
+
+    IEnumerator TutorialSequence(int x)
+    {
+        for (int i = 0; i < tutorialStopCount[x]; i++)
+        {
+            textBox.GetComponent<Text>().text = tutorialText[x][i];
+            yield return new WaitForSeconds(tutorialTimers[x][i]);
+
+        }
+        textBox.GetComponent<Text>().text = "";
+    }
+
 }
